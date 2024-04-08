0a0669877229b52bbb1d5ff8d8fd8b0cf56e5926
Bode <bode.sim@thundersoft.com>
2024-04-08 16:18:53 +0900

    2024-04-08 initial commit

---
+++ app/src/main/java/com/example/minglethedogs/auth/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.minglethedogs.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.minglethedogs.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
